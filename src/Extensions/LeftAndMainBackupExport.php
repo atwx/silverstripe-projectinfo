c7bc822a4ba415eeb228dcf34839775586feac4c
Andy Adiwidjaja <adiwidjaja@gmail.com>
2024-04-03 16:35:33 +0200

    Working assets download.

--- src/Extensions/LeftAndMainBackupExport.php
+++ src/Extensions/LeftAndMainBackupExport.php
@@ -5,14 +5,19 @@ namespace Atwx\ProjectInfo\Extensions;
 use SilverStripe\Core\Environment;
 use SilverStripe\Admin\LeftAndMainExtension;
 use SilverStripe\ORM\DB;
+use ZipArchive;
+use RecursiveIteratorIterator;
+use RecursiveDirectoryIterator;
+
 
 class LeftAndMainBackupExport extends LeftAndMainExtension
 {
     private static $allowed_actions = array(
-        'doBackup'
+        'doBackup',
+        'doDownloadAssets',
     );
 
-    public function doBackup(){
+    public function doBackup() {
         $host = Environment::getEnv('SS_DATABASE_SERVER');
         $user = Environment::getEnv('SS_DATABASE_USERNAME');
         $pass = Environment::getEnv('SS_DATABASE_PASSWORD');
@@ -34,5 +39,43 @@ class LeftAndMainBackupExport extends LeftAndMainExtension
         readfile($file);
         unlink($file);
         exit;
+    }
+
+    public function doDownloadAssets() {
+        // Initialize archive object
+        $zip = new ZipArchive();
+        $zip->open('assets.zip', ZipArchive::CREATE | ZipArchive::OVERWRITE);
+
+        $rootPath = ASSETS_PATH;
+
+        // Create recursive directory iterator
+        /** @var SplFileInfo[] $files */
+        $files = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($rootPath),
+            RecursiveIteratorIterator::LEAVES_ONLY
+        );
+
+        foreach ($files as $file)
+        {
+            // Skip directories (they would be added automatically)
+            if (!$file->isDir())
+            {
+                // Get real and relative path for current file
+                $filePath = $file->getRealPath();
+                $relativePath = substr($filePath, strlen($rootPath) + 1);
+
+                // Add current file to archive
+                $zip->addFile($filePath, $relativePath);
+            }
+        }
+
+        $zip->close();
+        header('Content-Description: File Transfer');
+        header('Content-Disposition: attachment; filename="assets.zip"');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Content-Length: ' . filesize('assets.zip'));
+        readfile('assets.zip');
+        unlink('assets.zip');
     }
 }
